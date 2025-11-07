e28f1e20606eb9671a87b3a34ec4c96b86ebff52
OishiMugicha <yutaro.st1192@gmail.com>
2025-11-07 23:33:25 +0900

    ゲームの状態を表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+import type { History } from './types/game'
+import { currentTurn, currentPot, currentFstStack, currentSndStack } from './utils/gameCalculations'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [history] = useState<History>({
+    playerIds: ['P1', 'P2'],
+    fstHand: 0.14,
+    sndHand: 0.83,
+    fstInitialStack: 1,
+    sndInitialStack: 1,
+    actions: [{'type': 'Bet', 'amount': 0.3}, {'type': 'Raise', 'amount': 0.5}],
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1>Zero-One Game</h1>
+        <section>
+          <h2>ハンド</h2>
+          <div>fstHand: {history.fstHand}</div>
+          <div>sndHand: {history.sndHand}</div>
+        </section>
+        <section>
+          <h2>ターン</h2>
+          <div>{currentTurn(history) === 'fst' ? '先手' : '後手'}</div>
+        </section>
+        <section>
+          <h2>ポット</h2>
+          <div>{currentPot(history)}</div>
+        </section>
+        <section>
+          <h2>スタック</h2>
+          <div>fstStack: {currentFstStack(history)}</div>
+          <div>sndStack: {currentSndStack(history)}</div>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
