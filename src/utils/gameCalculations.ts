5a70a446abd29ccb1211d212b872bf355196c638
OishiMugicha <yutaro.st1192@gmail.com>
2025-11-07 20:52:08 +0900

    getAvailableActionsの実装

--- src/utils/gameCalculations.ts
+++ src/utils/gameCalculations.ts
@@ -1,9 +1,9 @@
-import type { GameState, GameHistory, Turn, Action } from '../types/game';
+import type { GameState, GameHistory, Turn, ActionType } from '../types/game';
 
 /**
  * 最初のプレイヤーの現在のスタックを計算
  */
-export function calculateFstStack(history: GameHistory): number {
+export function fstStack(history: GameHistory): number {
   let fstStack = history.fstInitialStack;
   let sndStack = history.sndInitialStack;
   let lastBetAmount = 0;
@@ -45,7 +45,7 @@ export function calculateFstStack(history: GameHistory): number {
 /**
  * 2番目のプレイヤーの現在のスタックを計算
  */
-export function calculateSndStack(history: GameHistory): number {
+export function sndStack(history: GameHistory): number {
   let fstStack = history.fstInitialStack;
   let sndStack = history.sndInitialStack;
   let lastBetAmount = 0;
@@ -87,22 +87,42 @@ export function calculateSndStack(history: GameHistory): number {
 /**
  * ポットの合計を計算
  */
-export function calculatePot(history: GameHistory): number {
-  const fstStack = calculateFstStack(history);
-  const sndStack = calculateSndStack(history);
+export function pot(history: GameHistory): number {
   return (
     history.fstInitialStack +
     history.sndInitialStack -
-    fstStack -
-    sndStack
+    fstStack(history) -
+    sndStack(history)
   );
 }
 
 /**
  * 現在のターンを計算
  */
-export function calculateTurn(history: GameHistory): Turn {
+export function currentTurn(history: GameHistory): Turn {
   return history.actions.length % 2 === 0 ? 'fst' : 'snd';
+}
+
+export function fstBetAmount(history: GameHistory): number {
+  let lastBetAmount = 0;
+  for (let i = 0; i < history.actions.length; i++) {
+    const action = history.actions[i];
+    if (action.type === 'Bet' || action.type === 'Raise') {
+      lastBetAmount = action.amount;
+    }
+  }
+  return lastBetAmount;
+}
+
+export function sndBetAmount(history: GameHistory): number {
+  let lastBetAmount = 0;
+  for (let i = 0; i < history.actions.length; i++) {
+    const action = history.actions[i];
+    if (action.type === 'Bet' || action.type === 'Raise') {
+      lastBetAmount = action.amount;
+    }
+  }
+  return lastBetAmount;
 }
 
 export function getGameState(history: GameHistory): GameState {
@@ -125,3 +145,28 @@ export function getGameState(history: GameHistory): GameState {
     throw new Error('Invalid action type');
   }
 }
+
+export function getAvailableActions(history: GameHistory): ActionType[] {
+  let turn = currentTurn(history);
+  let allinRequired = false;
+  if (turn === 'fst') {
+    allinRequired = sndBetAmount(history) >= fstStack(history);
+  } else {
+    allinRequired = fstBetAmount(history) >= sndStack(history);
+  }
+  
+  if (getGameState(history) === 'Init' || getGameState(history) === 'Checked') {
+    return ['Check', 'Bet', 'Allin'];
+  } else if (getGameState(history) === 'Betted') {
+    if (allinRequired) {
+      return ['Fold', 'Call'];
+    } else {
+      return ['Fold', 'Call', 'Bet', 'Allin'];
+    }
+  } else if (getGameState(history) === 'Folded' || getGameState(history) === 'Showdown') {
+    return [];
+  } else {
+    throw new Error('Invalid game state');
+  }
+}
+
